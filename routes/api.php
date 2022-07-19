02d014bf1ea30485d10dfb6b5fc76838aa6763de
Andika Octavia <andhika@actdisain.com>
2022-07-19 16:33:50 +0700

    add jwt login, register, tokenjwt

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [App\Http\Controllers\UserController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\UserController::class, 'login']);
+Route::get('/book', [App\Http\Controllers\BookController::class, 'book'])->name('book');
+
+Route::get('/bookall', [App\Http\Controllers\BookController::class, 'bookAuth'])->middleware('jwt.verify');
+Route::get('/user', [App\Http\Controllers\UserController::class, 'getAuthenticatedUser'])->middleware('jwt.verify');
